4fdebce7b3cc02ddb57a93e20e3917196d96f43b
jxmot <webdev@stupiddog.net>
2021-01-02 09:53:33 -0600

    comments

--- getfqdnip.sh
+++ getfqdnip.sh
@@ -4,10 +4,13 @@
 #   getfqdnip.sh (this script file)
 #   getfqdnip.php
 #   readfqdndata.php
-#   fqdndata.php
+#   fqdnvars.php
 #
 # Its purpose is to obtain the IP address and save it in 
 # a JSON file by calling getfqdnip.php
+# 
+# The paths used below may be different depending on the
+# the platform you are using. 
 #
 # This command should be run as a CRON job at an interval 
 # of once per day.
