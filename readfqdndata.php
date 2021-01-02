ea1ac8a3690f0489c7c23e8395da71d0bf1e257b
jxmot <webdev@stupiddog.net>
2021-01-02 02:04:19 -0600

    comments

--- readfqdndata.php
+++ readfqdndata.php
@@ -1,4 +1,9 @@
 <?php
+// readfqdnip.php : reads the JSON object saved in 
+// the ?????.json file and saves it to a variable.
+//
+// Where: ????? is the file name that was constructed
+// and saved in FQDNFILE
 require_once dirname(__FILE__).'/fqdnvars.php';
 $fqdndata = json_decode(file_get_contents(FQDNFILE));
 ?>
