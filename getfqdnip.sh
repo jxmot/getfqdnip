692b03b6604143acba3a03d645c513798f784436
jxmot <webdev@stupiddog.net>
2021-01-03 01:27:33 -0600

    modified path for getfqdnip.php

--- getfqdnip.sh
+++ getfqdnip.sh
@@ -2,9 +2,9 @@
 # This script is part of a utility, which contains:
 #
 #   getfqdnip.sh (this script file)
+#   fqdnvars.php
 #   getfqdnip.php
 #   readfqdndata.php
-#   fqdnvars.php
 #
 # Its purpose is to obtain the IP address and save it in 
 # a JSON file by calling getfqdnip.php
@@ -14,4 +14,4 @@
 #
 # This command should be run as a CRON job at an interval 
 # of once per day.
-/usr/local/bin/php /home/$USER/public_html/data/getfqdnip.php
+/usr/local/bin/php /home/$USER/public_html/php/getfqdnip.php
